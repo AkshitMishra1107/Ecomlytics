d417c823c3634b0b60e1ffc738dea35f1a3633f4
Akshit Mishra <akshitmishra117@gmail.com>
2026-04-21 00:49:43 +0530

    Update server2.js

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//////////////////////
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+///////////////////////
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT || 3306,
